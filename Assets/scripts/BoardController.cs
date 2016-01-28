cf88641291888414216aeaaf0abcd92612357bc3
pablo jacob delgado <fluffydemc@gmail.com>
2016-01-28 11:07:25 -0800

    code

---
+++ Assets/scripts/BoardController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoardController : MonoBehaviour {
+
+	public int columns;
+	public int rows;
+
+	public GameObject floor;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/Gameboard.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	public static Instance;
+
+	void Awake () {
+		if (instance != null && instance != this) {
+			{
+				Destroy(gameObject); 
+				return;
+			}
+
+		Instance = this;
+		DontDestroyOnLoad (gameObject);
+	}
+	
+
+	void Update () {
+	
+	}
+}
